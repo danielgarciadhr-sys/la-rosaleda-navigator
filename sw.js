ae9a1dbecf63e2e4ef4168ef8d75b4b11cc2cd1d
danielgarciadhr-sys <danielgarciadhr@gmail.com>
2026-09-24 12:50:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='rosaleda-v3';const A=['./','./index.html','./manifest.webmanifest','./mapa-la-rosaleda.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='rosaleda-v2';const A=['./','./index.html','./manifest.webmanifest','./mapa-la-rosaleda.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
